848468dc717f58de18a2889fae25333e8a955e1b
Jacob Bunting <jacobbunting260@gmail.com>
2018-02-11 22:26:33 -0500

    Added venue and date classes

--- src/base-app/baseapp.h
+++ src/base-app/baseapp.h
@@ -2,6 +2,7 @@
 #define BASEAPP_H
 
 #include "base-app_global.h"
+#include <string>
 
 class BASEAPPSHARED_EXPORT BaseApp
 {
@@ -11,3 +12,28 @@ public:
 };
 
 #endif // BASEAPP_H
+
+#ifndef date.h
+#define date.h
+
+class date{
+    public:
+        bool setDate(int m, int d, int y);
+        bool setTime(int h, int m);
+    private:
+        int day, month, year, hour, minute;
+}
+#endif
+
+#ifndef venue.h
+#define venue.h
+
+class venue{
+    public
+        void setAddress(string a);
+        void setPhoneNumber(int ns);
+    private:
+        string address;
+        int phoneNumber;
+}
+#endif
