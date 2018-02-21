4791820534eb5ec1000cece64d463399b9660aef
VoxOel <antchroel@yahoo.com>
2018-02-21 18:57:55 -0500

    chnaged minor things to line up with front end

--- db/createtables.sql
+++ db/createtables.sql
@@ -1,16 +1,18 @@
-/* Implemented by Kyle and Anthony. */
+/* Implemented by Anthony and Kyle. */
 create table User_basic(		--unverified access profile
-	id LONG,
-	userType ENUM('BASIC','PROFILE','HOST'),
-	username CHAR(20) PRIMARY KEY,
-	password CHAR(20),
-	verified BOOLEAN
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	userType ENUM('BASIC','PROFILE','HOST') NOT NULL,
+	username CHAR(20) NOT NULL,
+	password CHAR(20) NOT NULL,
+	verified BOOLEAN NOT NULL
 );
 
-create table User_Profile(		--std profile, created after account verification
-	id_user LONG,
-	firstName VARCHAR(40),
-	lastName VARCHAR(40),
+create table User_Profile(		--std profile
+	id_user INT NOT NULL UNIQUE,
+	firstName VARCHAR(40) NOT NULL,
+	lastName VARCHAR(40) NOT NULL,
+	birtday DATE NOT NULL,
+	email VARCHAR(40),
 	gender ENUM('MALE','FEMALE','OTHER'),
 	otherGender VARCHAR(40),
 	orientation VARCHAR(3),		--space for 3 char flags, m,f,o. used to say interest in male, female, and or other
@@ -19,64 +21,62 @@ create table User_Profile(		--std profile, created after account verification
 );
 
 create table User_Host(			--business/host profile
-	id_user LONG,
-	displayName VARCHAR(80),
-	businessName VARCHAR(40)
+	id_user INT NOT NULL UNIQUE,
+	displayName VARCHAR(80) NOT NULL,
+	businessName VARCHAR(40) NOT NULL,
+	bio TEXT
 );
 
-create table Event_Single(		--previously titled interest, changed for clarity
-	id LONG,					--used for single day, one-off events
-	displayName VARCHAR(20),	
-	id_category LONG,
-	id_venue LONG,
-	dateStart DATE,
-	timeStart TIME,
-	timeEnd TIME,
-	description TEXT
+create table Event(				--previously titled interest, changed for clarity
+	id INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	standardOperation BOOLEAN NOT NULL,	--if true, this event represents the business' normal hours and procedure
+	recurring BOOLEAN NOT NULL,
+	displayName VARCHAR(20) NOT NULL,	
+	id_category INT NOT NULL,
+	id_venue INT NOT NULL,
+	dateStart DATE NOT NULL,
+	dateEnd DATE,-----------------used for long events, otherwise ignored
+	timeStart TIME NOT NULL,
+	timeEnd TIME NOT NULL,
+	recurringType ENUM('DAILY','WEEKLY','MONTHLY','FIRST','SECOND','THIRD','FOURTH','LAST'), 	--first,last etc used for first friday, last tuesday, etc
+	recurringDay INT,	   /*	changes based on recurType: 
+							*		daily, first-last: 1-7 to signify day of week (starting sunday as 1), 0 unused
+							*		monthly day 1-(29/30/31) to signify day of month
+							*/
+	description TEXT			
 );
 
-create table Event_Recurring(	
-	id LONG,
-	displayName VARCHAR(20),
-	id_category LONG,
-	id_venue LONG,
-	recurringType ENUM('DAILY','WEEKLY','MONTHLY','FIRST','SECOND','THIRD','LAST'), 	--first,last etc used for first friday, last tuesday, etc
-	recurringDay INT,		--effect changes based on recurType: 
-	recurringTime TIME,		--^--daily, first-last: 1-7 to signify day of week (starting monday)
-	description TEXT		--^--monthly day 1-(29/30/31) valid to determine day of month
-);
-
-create table Event_Long(		--for several day events like a fair or music festival
-	id LONG,					
-	displayName VARCHAR(20),	
-	id_category LONG,
-	id_venue LONG,
-	dateStart DATE,
-	dateEnd	DATE,
-	timeStart TIME,
-	timeEnd TIME,
+create table Event_Exceptions(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id_event INT NOT NULL,
+	dateException DATE NOT NULL,
 	description TEXT
 );
 
 create table Category(
-	id LONG,
-	id_
+	id INT NOT NULL ,
 	displayName VARCHAR(20)
-	
+);
+
+create table Subcategory(
+	id INT,
+	id_category INT,
+	displayName VARCHAR(20)
 );
 
 create table Venue(
-	id LONG,
+	id INT,
 	address VARCHAR(40),
-	phoneNumber VARCHAR(20)
+	phoneNumber VARCHAR(20),
 	entryFee DOUBLE,
+	description TEXT
 );
 
 create table Join_Date(
-	id LONG,
-	id_venue LONG,
-	id_userA LONG,
-	id_userB LONG,
+	id INT,
+	id_event INT,
+	id_userA INT,
+	id_userB INT,
 	confirmedA boolean,
 	confirmedB boolean
 );
