8458bc73b42e5feac5b90394e948b300b1f2e464
VoxOel <antchroel@yahoo.com>
2018-02-20 21:07:06 -0500

    Added id fields for tables missing, expanded some tables

--- db/createtables.sql
+++ db/createtables.sql
@@ -1,52 +1,82 @@
 /* Implemented by Kyle and Anthony. */
-create table User_basic(
+create table User_basic(		--unverified access profile
 	id LONG,
-	username CHAR(20),
+	userType ENUM('BASIC','PROFILE','HOST'),
+	username CHAR(20) PRIMARY KEY,
 	password CHAR(20),
 	verified BOOLEAN
 );
 
-create table User_Profile(
+create table User_Profile(		--std profile, created after account verification
 	id_user LONG,
-	firstName VARCHAR(20),
-	lastName VARCHAR(20)
+	firstName VARCHAR(40),
+	lastName VARCHAR(40),
+	gender ENUM('MALE','FEMALE','OTHER'),
+	otherGender VARCHAR(40),
+	orientation VARCHAR(3),		--space for 3 char flags, m,f,o. used to say interest in male, female, and or other
+	bio TEXT
 	
 );
 
-create table User_Profesional(
+create table User_Host(			--business/host profile
 	id_user LONG,
-	displayName VARCHAR(40),
+	displayName VARCHAR(80),
 	businessName VARCHAR(40)
 );
 
-create table Interest_Single(
-	displayName VARCHAR(20),
+create table Event_Single(		--previously titled interest, changed for clarity
+	id LONG,					--used for single day, one-off events
+	displayName VARCHAR(20),	
 	id_category LONG,
+	id_venue LONG,
+	dateStart DATE,
+	timeStart TIME,
+	timeEnd TIME,
 	description TEXT
-	
 );
 
-create table Interest_Recurring(
+create table Event_Recurring(	
+	id LONG,
 	displayName VARCHAR(20),
 	id_category LONG,
+	id_venue LONG,
+	recurringType ENUM('DAILY','WEEKLY','MONTHLY','FIRST','SECOND','THIRD','LAST'), 	--first,last etc used for first friday, last tuesday, etc
+	recurringDay INT,		--effect changes based on recurType: 
+	recurringTime TIME,		--^--daily, first-last: 1-7 to signify day of week (starting monday)
+	description TEXT		--^--monthly day 1-(29/30/31) valid to determine day of month
+);
+
+create table Event_Long(		--for several day events like a fair or music festival
+	id LONG,					
+	displayName VARCHAR(20),	
+	id_category LONG,
+	id_venue LONG,
+	dateStart DATE,
+	dateEnd	DATE,
+	timeStart TIME,
+	timeEnd TIME,
 	description TEXT
 );
 
 create table Category(
 	id LONG,
+	id_
 	displayName VARCHAR(20)
 	
 );
 
 create table Venue(
-	id_venue LONG,
+	id LONG,
 	address VARCHAR(40),
 	phoneNumber VARCHAR(20)
+	entryFee DOUBLE,
 );
 
 create table Join_Date(
-	userA VARCHAR(20),
+	id LONG,
+	id_venue LONG,
+	id_userA LONG,
+	id_userB LONG,
 	confirmedA boolean,
-	userB VARCHAR(20),
 	confirmedB boolean
 );
