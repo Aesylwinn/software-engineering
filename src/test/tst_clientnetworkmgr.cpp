7a74677daa18a5107ff67b9fe851f17cd4649ee4
Kyle Cooley <kyleacooley@gmail.com>
2018-02-23 10:05:45 -0500

    Improve ClientNetworkMgr test

--- src/test/tst_clientnetworkmgr.cpp
+++ src/test/tst_clientnetworkmgr.cpp
@@ -4,6 +4,8 @@
 
 // Qt
 #include <QCoreApplication>
+#include <QTcpServer>
+#include <QTcpSocket>
 #include <QTimer>
 
 // Class being tested
@@ -15,11 +17,29 @@ using namespace base;
 TEST(base, ClientNetworkMgr_connect_case1) {
     // Workaround for const char* to char* issue
     char AppName[] = { 'T', 'S', 'T', '\n' };
+    int Port = 1923;
 
     // Set up an event loop
     int argc = 1;
     char* argv[2] = { AppName, NULL };
     QCoreApplication app(argc, argv);
+
+    // Create a dummy socket to listen for the connection
+    QTcpServer* server = new QTcpServer(&app);
+    server->listen(QHostAddress::Any, Port);
+    // Connection test
+    bool wasConnected = false;
+    bool wasRecieved = false;
+    QObject::connect(server, &QTcpServer::newConnection, [&]() {
+        wasConnected = true;
+        QTcpSocket* socket = server->nextPendingConnection();
+        if (socket) {
+            QObject::connect(socket, &QTcpSocket::readyRead, [&]() {
+                wasRecieved = true;
+                app.quit();
+            });
+        }
+    });
 
     // Set up network
     ClientNetworkMgr* client = new ClientNetworkMgr(&app);
@@ -31,12 +51,14 @@ TEST(base, ClientNetworkMgr_connect_case1) {
         wasSent = true;
     });
     // Connect
-    client->connect("::1", 1942);
+    client->connect("::1", Port);
 
     // Wait no more than 1 second
     QTimer::singleShot(1000, &app, SLOT(quit()));
 
     // Run
     ASSERT_EQ(app.exec(), 0);
+    ASSERT_TRUE(wasConnected);
     ASSERT_TRUE(wasSent);
+    ASSERT_TRUE(wasRecieved);
 }
