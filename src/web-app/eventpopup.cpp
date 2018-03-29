aa7f60d1c2c55b0c9c80eb4b17ae4f4ee7355f94
ParkerRnlds <pjr15b@my.fsu.edu>
2018-03-29 10:21:34 -0300

    added createhost stuff

--- src/web-app/eventpopup.cpp
+++ src/web-app/eventpopup.cpp
@@ -1,6 +1,7 @@
 /*Written by Darius and Parker*/
 #include "eventpopup.h"
 #include "ui_eventpopup.h"
+#include "interestdata.h"
 
 eventPopUp::eventPopUp(QWidget *parent) :
     QDialog(parent),
@@ -9,6 +10,7 @@ eventPopUp::eventPopUp(QWidget *parent) :
     ui->setupUi(this);
     connect(ui->yes, SIGNAL(clicked()), this, SLOT(openingCalendar()));
     connect(ui->no, SIGNAL(clicked()), this, SLOT(openingCalendar()));
+    
 }
 
 
@@ -25,4 +27,18 @@ void eventPopUp::openingCalendar()
         ui->recurringDate->setEnabled(true);
     else
         ui->recurringDate->setEnabled(false);
+}
+
+void eventPopUp::setHostInfo()
+{
+    interestData *info;
+    for(int i = 0; i < 6; i++)
+        for(int j = 0; j < 6; j++){
+            QString item = ui->category->itemAt(i,y);
+        }
+    QString item = ui->category->it
+    ui->category->isItemSelected()
+    info->createEvent(ui->eventName_2->text(), ,ui->descriptionText->toPlainText());
+    
+    
 }
