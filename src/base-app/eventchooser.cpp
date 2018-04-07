2c4b4066f69129202789e2ac66d1f98d6729f28f
Jacob Bunting <jacobbunting260@gmail.com>
2018-04-07 11:41:51 -0400

    EventChooser implemented and tested

--- src/base-app/eventchooser.cpp
+++ src/base-app/eventchooser.cpp
@@ -1,4 +1,4 @@
-/*Implemented by Jake*/
+/*Implemented by Jake and Kyle*/
 
 #include "eventchooser.h"
 
@@ -7,21 +7,49 @@ namespace base{
 
     QVector<Event> EventChooser::narrow(QVector<QString> interests, QVector<Event> list, qint32 numEvents)
     {
+        //check to see if requested number is too large
         if (numEvents > list.size()) {
             numEvents = list.size();
         }
 
-        QVector<Event> newList;
+        QVector<Event> matches;
+        QVector<Event> random;
+        QVector<Event> output;
+
         qint32 index;
 
-        do{
-            index = qrand() % list.size();
+        //populate lists
+        for (int i = 0; i < list.size(); i++){
+            if (interests.contains(list[i].getCategory()))
+                matches.push_back(list[i]);
+            else
+                random.push_back(list[i]);
+        }
 
-            if (!newList.contains(list[index])) {
-                newList.push_back(list[index]);
-            }
-        } while(newList.size() < numEvents);
+        //checking for enough matches/random to populate the final list
+        int numMatches = numEvents * 7 / 10;
+        int numRandom = numEvents - numMatches;
+        if (numMatches > matches.size()){
+            numRandom += numMatches - matches.size();
+            numMatches = numEvents - numRandom;
+        }
+        if (numRandom > random.size()){
+            numMatches += numRandom - random.size();
+            numRandom = numEvents - numMatches;
+        }
 
-        return newList;
+        //fill 70% of final list with matches
+        while(output.size() < numMatches && !matches.isEmpty()){
+            index = qrand() % matches.size();
+            output.push_back(matches[index]);
+            matches.remove(index);
+        }
+        //fill the rest with random events
+        while(output.size() < numEvents && !random.isEmpty()){
+            index = qrand() % random.size();
+            output.push_back(random[index]);
+            random.remove(index);
+        }
+        return output;
     }
 }
